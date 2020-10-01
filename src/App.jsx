eab90ee1ac088ab46bd4a3304673a81ae3d324b3
Amogh Jalan <amoghjalan2005@gmail.com>
2020-10-01 17:28:20 +0530

    Added Basic Layout Added Container.jsx Added Board.jsx Added Footer.jsx Added Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import Container from "./components/Container";
 function App() {
   return (
     <div className="App">
-      <h1>Path Visualizer</h1>
+      <Container />
     </div>
   );
 }
---
+++ src/components/Board.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Container() {
+  return (
+    <div
+      style={{
+        minHeight: 400,
+        margin: 20,
+        border: "4px solid black"
+      }}
+    >
+      Grid
+    </div>
+  );
+}
