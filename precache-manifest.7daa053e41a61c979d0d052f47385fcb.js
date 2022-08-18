c44ea39bb9fcff2caebc698ffedc19331632c11c
amoghjalan <amoghjalan2005@gmail.com>
2022-08-18 14:23:24 +0530

    Updates

---
+++ precache-manifest.7daa053e41a61c979d0d052f47385fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e3471866b1f6ebbbe18d3aa59f5fc3",
+    "url": "/GraphIn/index.html"
+  },
+  {
+    "revision": "2a1b2bf5c4a0c21ef4f2",
+    "url": "/GraphIn/static/css/main.ac1f6430.chunk.css"
+  },
+  {
+    "revision": "7e666810af1514eebcaa",
+    "url": "/GraphIn/static/js/2.73a848eb.chunk.js"
+  },
+  {
+    "revision": "c24bd15efa6a02fc028f6ddc87879af3",
+    "url": "/GraphIn/static/js/2.73a848eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1b2bf5c4a0c21ef4f2",
+    "url": "/GraphIn/static/js/main.10961be6.chunk.js"
+  },
+  {
+    "revision": "b30ed11b4d46c35fa2b6",
+    "url": "/GraphIn/static/js/runtime-main.7ec05d8f.js"
+  }
+]);
